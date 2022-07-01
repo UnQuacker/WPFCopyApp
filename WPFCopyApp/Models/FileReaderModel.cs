ef150a26e0601a0ada24d964dd8a3ec373d75c9a
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-07-01 12:41:35 +0600

    DLL added

--- WPFCopyApp/Models/FileReaderModel.cs
+++ WPFCopyApp/Models/FileReaderModel.cs
@@ -6,12 +6,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WPFCopyApp.ViewModels;
+using System.Runtime.InteropServices;
 
 namespace WPFCopyApp.Models
 {
     public class FileReaderModel
     {
         public string _content;
+        public StringBuilder temp;
+
+        [DllImport("RawDataDLL.dll")]
+        public static extern IntPtr Create();
+
+        [DllImport("RawDataDLL.dll")]
+        public static extern void RunTest(IntPtr dll, StringBuilder stringBuilder, int sector);
 
         public FileReaderModel()
         {
@@ -19,12 +27,19 @@ namespace WPFCopyApp.Models
 
         public void ReadText(FileReaderViewModel fileReaderViewModel)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "txt files (*.txt)|*.txt";
-            if (openFileDialog.ShowDialog() != true) { return; }
-            fileReaderViewModel.test = openFileDialog.FileName;
-            fileReaderViewModel.content = File.ReadAllText(openFileDialog.FileName);
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            //if (openFileDialog.ShowDialog() != true) { return; }
+            //fileReaderViewModel.test = openFileDialog.FileName;
+            //fileReaderViewModel.content = File.ReadAllText(openFileDialog.FileName);
 
+            temp = new StringBuilder();
+
+            fileReaderViewModel.test = "Opened with dll";
+            IntPtr dll = Create();
+            RunTest(dll, temp, 118);
+            fileReaderViewModel.content = temp.ToString();
         }
+
     }
 }
